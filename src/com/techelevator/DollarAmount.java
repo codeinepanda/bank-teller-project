8eacf7a8e5e3d3368a1324bc61398fb5fea42a17
Hayden Thompson <redechox@gmail.com>
2016-02-08 16:33:58 -0500

    got stuck on plus and minus

--- src/com/techelevator/DollarAmount.java
+++ src/com/techelevator/DollarAmount.java
@@ -3,50 +3,57 @@ package com.techelevator;
 public class DollarAmount {
 
 	private long totalAmountInCents;
-	
-	public  DollarAmount ( long totalAmountInCents ) {
+
+	public DollarAmount(long totalAmountInCents) {
 		this.totalAmountInCents = totalAmountInCents;
 	}
-	
-	public DollarAmount ( long dollars , int cents) {
+
+	public DollarAmount(long dollars, int cents) {
 		this.totalAmountInCents = (dollars * 100) + cents;
 	}
-	
-	public int getCents () {
-		return (int) ( totalAmountInCents % 100 );
+
+	public int getCents() {
+		return (int) (totalAmountInCents % 100);
 	}
-	
-	public long getDollars () {
+
+	public long getDollars() {
 		return (totalAmountInCents) / 100;
 	}
-	
-	public boolean isEqualTo (DollarAmount amountToCompare){
-		return ( (totalAmountInCents) == amountToCompare.totalAmountInCents );					
+
+	public boolean isEqualTo(DollarAmount amountToCompare) {
+		return ((totalAmountInCents) == amountToCompare.totalAmountInCents);
 	}
-	
-	public boolean isLessThan (DollarAmount amountToCompare) {
+
+	public boolean isLessThan(DollarAmount amountToCompare) {
 		return totalAmountInCents < amountToCompare.totalAmountInCents;
 	}
-	
-	public boolean isGreaterThan (DollarAmount amountToCompare) {
+
+	public boolean isGreaterThan(DollarAmount amountToCompare) {
 		return totalAmountInCents > amountToCompare.totalAmountInCents;
 	}
-	
-	public boolean isNegative () {
+
+	public boolean isNegative() {
 		return totalAmountInCents < 0;
 	}
-	
-	//public DollarAmount plus (DollarAmount amountToAdd) {
-		//DollarAmount sum = new DollarAmount(totalAmountInCents);
-		//sum =  sum.getDollars()*100 + sum.getCents() + amountToAdd.getDollars()*100 + amountToAdd.getCents();
-	//}
-	
-	public void minus (DollarAmount amountToSubtract) {
+
+	public DollarAmount plus(DollarAmount amountToAdd) {
+		//long sumOf = 0;
+		//sumOf = totalAmountInCents + amountToAdd.totalAmountInCents;
+		//DollarAmount sumPlus = new DollarAmount(sumOf);
+		//return sumPlus;
+		// sum.getDollars()*100 + sum.getCents() + amountToAdd.getDollars()*100
+		// + amountToAdd.getCents();
+		//DollarAmount sum = DollarAmount myDollars(DollarAmount otherDollars)
+		//DollarAmount sum = new DollarAmount(this.totalAmountInCents + amountToAdd);
+		
+		DollarAmount sum = new DollarAmount(amountToAdd.totalAmountInCents + this.totalAmountInCents);
+		return sum;
+		//totalAmountInCents = sum;
+		
+	}
+
+	public void minus(DollarAmount amountToSubtract) {
 		totalAmountInCents = totalAmountInCents - amountToSubtract.totalAmountInCents;
 	}
 
-
-	
-	
 }
-	
--- src/com/techelevator/DollarAmountTest.java
+++ src/com/techelevator/DollarAmountTest.java
@@ -3,39 +3,47 @@ package com.techelevator;
 public class DollarAmountTest {
 
 	public static void main(String[] args) {
-		
-		
-	DollarAmount myDollars = new DollarAmount(575);
-	
+
+		DollarAmount myDollars = new DollarAmount(575);
+
 		System.out.println(myDollars.getCents());
 		System.out.println(myDollars.getDollars());
-	
-	System.out.println();
+
+		System.out.println();
 
 		DollarAmount otherDollars = new DollarAmount(785);
-	
+
 		System.out.println(myDollars.isEqualTo(otherDollars));
-	
-	System.out.println();
-	
+
+		System.out.println();
+
 		System.out.println(myDollars.isLessThan(otherDollars));
-	
-	System.out.println();
-	
+
+		System.out.println();
+
 		System.out.println(myDollars.isGreaterThan(otherDollars));
-	
-	System.out.println();
-	
+
+		System.out.println();
+
 		System.out.println(myDollars.isNegative());
-	
-	System.out.println();
-	
-		//myDollars.plus(otherDollars);
-		//System.out.println( (myDollars.getDollars()*100) + myDollars.getCents() );
-	
-	
-	
-	
+
+
+		myDollars.plus(otherDollars);
+		System.out.println((myDollars.getDollars() * 100) + myDollars.getCents() + " Should be 13.60");
+		System.out.println(myDollars.plus(otherDollars));
+		
+		
+		//myDollars.minus(otherDollars);
+		//System.out.println((myDollars.getDollars() * 100) + myDollars.getCents());
+		//myDollars.minus(otherDollars);
+		//System.out.println((myDollars.getDollars() * 100) + myDollars.getCents());
+
+		//DollarAmount newDollars = new DollarAmount(250);
+		
+		//newDollars.plus(myDollars);
+
+
+		
 	}
 
 }
