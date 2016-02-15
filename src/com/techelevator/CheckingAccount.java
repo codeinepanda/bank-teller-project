17837e4a2a747e619a8b73671de12b2606e772e6
Hayden Thompson <redechox@gmail.com>
2016-02-15 16:17:24 -0500

    tested the Checking Account, now on to DollarAmount

--- src/com/techelevator/CheckingAccount.java
+++ src/com/techelevator/CheckingAccount.java
@@ -3,8 +3,8 @@ package com.techelevator;
 public class CheckingAccount extends BankAccount {
 
 	private static final DollarAmount OVERDRAFT_FEE = new DollarAmount(1000);
-	private static final DollarAmount OVERDRAFT_LIMIT = new DollarAmount(-10001);
-	private static final DollarAmount ZERO = new DollarAmount(-01);
+	private static final DollarAmount OVERDRAFT_LIMIT = new DollarAmount(-10000);
+	private static final DollarAmount ZERO = new DollarAmount(0);
 	
 	public CheckingAccount(BankCustomer accountHolder , String accountNumber /*, DollarAmount balance*/) {  // need to update to align with BankCustomer?
 		super(accountHolder , accountNumber /*, balance*/);
@@ -14,11 +14,11 @@ public class CheckingAccount extends BankAccount {
 	public void transfer (BankAccount destinationAccount, DollarAmount transferAmount) {
 		DollarAmount balance = this.getBalance();
 		
-		if ((balance.minus(transferAmount).isGreaterThan(ZERO))) {
+		if ((balance.minus(transferAmount).isGreaterThanOrEqualTo(ZERO))) {
 			this.withdraw(transferAmount);
 			destinationAccount.deposit(transferAmount);
 			
-		} else if (balance.minus(transferAmount.plus(OVERDRAFT_FEE)).isGreaterThan(OVERDRAFT_LIMIT)) {
+		} else if (balance.minus(transferAmount.plus(OVERDRAFT_FEE)).isGreaterThanOrEqualTo(OVERDRAFT_LIMIT)) {
 			this.withdraw(transferAmount.plus(OVERDRAFT_FEE));
 			destinationAccount.deposit(transferAmount);	
 		} 
@@ -26,16 +26,16 @@ public class CheckingAccount extends BankAccount {
 	}
 	
 	
-	public DollarAmount withdraw (DollarAmount amountToWithdraw) {  
+	public DollarAmount withdrawal (DollarAmount amountToWithdraw) {  
 		DollarAmount balance = this.getBalance();
 		
-		if (balance.minus(amountToWithdraw).isGreaterThan(ZERO)) {
-			super.withdraw(amountToWithdraw);
-			return super.getBalance();
+		if (balance.minus(amountToWithdraw).isGreaterThanOrEqualTo(ZERO)) {
+			return super.withdraw(amountToWithdraw);
+			//return super.getBalance();
 			
-		} else if (balance.minus(amountToWithdraw.plus(OVERDRAFT_FEE)).isGreaterThan(OVERDRAFT_LIMIT)) {
-			super.withdraw(amountToWithdraw.plus(OVERDRAFT_FEE));
-			return super.getBalance();
+		} else if (balance.minus(amountToWithdraw.plus(OVERDRAFT_FEE)).isGreaterThanOrEqualTo(OVERDRAFT_LIMIT)) {
+			return super.withdraw(amountToWithdraw.plus(OVERDRAFT_FEE));
+			//return super.getBalance();
 			
 		} else {
 			return super.getBalance();
---
+++ test/com/techelevator/CheckingAccountTest.java
@@ -0,0 +1,131 @@
+package com.techelevator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+
+
+public class CheckingAccountTest {
+	
+	private CheckingAccount theCheckAccount;
+	private CheckingAccount jane2Account;
+	
+	@Before		// this annotation tells JUnit to run this method before every test (setup method will run before every test	
+	public void setup(){
+		BankCustomer  aCustomer = new BankCustomer("John Doe", "123 Main St", "123-456-7890");
+		theCheckAccount = new CheckingAccount(aCustomer, "2345678");
+		BankCustomer  bCustomer = new BankCustomer("Jane Doe", "321 Main St", "456-123-7890");
+		jane2Account = new CheckingAccount(bCustomer, "8765432");
+		// every test will start with this new CheckingAccount with a balance of 0
+	}
+
+	@Test
+	public void new_checking_account_has_starting_balance_of_0() {
+
+		DollarAmount zeroDollars = new DollarAmount(0);
+		Assert.assertEquals(zeroDollars, theCheckAccount.getBalance());
+	}
+
+	
+	@Test
+	public void deposit_increases_the_checking_balance_by_the_amount_deposited(){
+
+		DollarAmount amountDeposited = new DollarAmount(12345);
+		DollarAmount newBalance = theCheckAccount.deposit(amountDeposited);
+		
+		Assert.assertEquals(amountDeposited, newBalance);
+		Assert.assertEquals(amountDeposited, theCheckAccount.getBalance());
+	}
+	
+	
+	@Test
+	public void get_checking_holder_gives_account_holder_name(){
+		Assert.assertEquals("John Doe", theCheckAccount.getAccountHolder().getName());
+	}
+	
+	@Test
+	public void get_checking_holder_address_gives_account_holder_address(){
+		Assert.assertEquals("123 Main St", theCheckAccount.getAccountHolder().getAddress());
+	}
+	
+	@Test
+	public void get_checking_number_gives_account_number() {
+		Assert.assertEquals("2345678", theCheckAccount.getAccountNumber());
+	}
+	
+	@Test
+	public void withdrawal_from_account_with_balance_under_150_takes_service_charge(){
+		DollarAmount amountWithdraw = new DollarAmount (1000);
+		DollarAmount amountEnding = new DollarAmount (-2000);
+		
+		theCheckAccount.withdrawal(amountWithdraw);
+		
+		Assert.assertEquals(amountEnding, theCheckAccount.getBalance());
+	}
+	@Test
+	public void withdrawal_from_account_should_fail() {
+		DollarAmount amountWithdraw = new DollarAmount (9001);
+		theCheckAccount.withdrawal(amountWithdraw);
+
+		Assert.assertEquals(DollarAmount.ZERO_DOLLARS, theCheckAccount.getBalance());
+	}
+	@Test
+	public void withdrawal_from_account_should_withdraw() {
+		DollarAmount amountWithdraw = new DollarAmount (100);
+		theCheckAccount.deposit(amountWithdraw);
+		theCheckAccount.withdrawal(amountWithdraw);
+
+		Assert.assertEquals(DollarAmount.ZERO_DOLLARS, theCheckAccount.getBalance());
+	}
+	//#######Transfer######//
+	
+	@Test
+	public void transfer_should_transfer_amount_to_transfer_out_of_account(){
+		DollarAmount amountDeposited = new DollarAmount (25000);
+		DollarAmount amountTransfer = new DollarAmount (1000);
+		DollarAmount amountEnding = new DollarAmount (24000);
+		DollarAmount janeEnding = new DollarAmount (1000);
+		
+		theCheckAccount.deposit(amountDeposited);
+		theCheckAccount.transfer(jane2Account, amountTransfer);
+		
+		Assert.assertEquals(amountEnding, theCheckAccount.getBalance());
+		Assert.assertEquals(janeEnding, jane2Account.getBalance());
+	}
+	
+	@Test
+	public void transfer_from_account_with_just_service_fee(){
+		DollarAmount amountDeposited = new DollarAmount (10000);
+		DollarAmount amountTransfer = new DollarAmount (11000);
+		DollarAmount amountEnding = new DollarAmount (-2000);
+		DollarAmount janeEnding = new DollarAmount (11000);
+		
+		theCheckAccount.deposit(amountDeposited);
+		theCheckAccount.transfer(jane2Account, amountTransfer);
+		
+		Assert.assertEquals(amountEnding, theCheckAccount.getBalance());
+		Assert.assertEquals(janeEnding, jane2Account.getBalance());
+	}
+	
+	@Test
+	public void transfer_should_not_work_if_transfer_amount_is_greater_than_negative_$100(){
+		DollarAmount amountTransfer = new DollarAmount (10000);
+		
+		theCheckAccount.transfer(jane2Account, amountTransfer);
+		
+		
+		Assert.assertEquals(jane2Account.getBalance(), theCheckAccount.getBalance());
+	}
+	//###########//
+	
+	@Test
+	public void to_string_should_return_account_type_and_account_number(){
+		Assert.assertEquals("Checking Account 2345678", theCheckAccount.toString());
+	}
+
+	
+	
+}
